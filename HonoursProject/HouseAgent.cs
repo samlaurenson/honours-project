c6fb9de39c8829502827c33befd8e4ac0faf64ad
Sam Laurenson <40398765@live.napier.ac.uk>
2021-10-29 16:15:12 +0100

    Creating Dockerfile and integrating TravisCI

---
+++ HonoursProject/HouseAgent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ActressMas;
+
+namespace HonoursProject
+{
+    class HouseAgent : Agent
+    {
+        public override void Setup()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HonoursProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ActressMas;
+
+namespace HonoursProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = new EnvironmentMas();
+            var houseAg = new HouseAgent();
+            env.Add(houseAg, "house");
+            env.Start();
+        }
+    }
+}
