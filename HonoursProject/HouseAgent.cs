c60f1e9a1279d798d6ac41aee5546b346a917a10
Sam Laurenson <40398765@live.napier.ac.uk>
2021-12-05 21:30:00 +0000

    Integrating docker with travis

--- HonoursProject/HouseAgent.cs
+++ HonoursProject/HouseAgent.cs
@@ -10,6 +10,14 @@ namespace HonoursProject
         public override void Setup()
         {
             Console.WriteLine("Hello World!");
+            int i = 0;
+            while(true)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Stop();
+            //Environment.SimulationFinished();
         }
     }
 }
--- HonoursProject/Program.cs
+++ HonoursProject/Program.cs
@@ -3,14 +3,30 @@ using ActressMas;
 
 namespace HonoursProject
 {
+    public class TestMe
+    {
+        public int func(int t)
+        {
+            return t + 1;
+        }
+
+        public int func2(int t)
+        {
+            return t - 1;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            TestMe t = new TestMe();
+            int test = t.func(2);
+            int test2 = t.func2(2);
             var env = new EnvironmentMas();
             var houseAg = new HouseAgent();
             env.Add(houseAg, "house");
             env.Start();
+            //Console.ReadLine();
         }
     }
 }
