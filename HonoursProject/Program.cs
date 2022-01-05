ebe25f23314f263ebf0349300f5497e745775a97
Sam Laurenson <40398765@live.napier.ac.uk>
2022-01-05 15:24:14 +0000

    Implementing demand curves so that agents can request and receive time slot allocations

--- HonoursProject/Program.cs
+++ HonoursProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ActressMas;
 using HonoursProject.behaviours;
 
@@ -20,10 +22,55 @@ namespace HonoursProject
     {
         static void Main(string[] args)
         {
-            /*TestMe t = new TestMe();
-            int test = t.func(2);
-            int test2 = t.func2(2);*/
-            var env = new EnvironmentMas();
+            //Size of array is causing issues when using a double[,], so a list will be used instead -- ideally move this in to a config file
+            List<List<double>> DEMAND_CURVE = new List<List<double>>()
+            {
+                new List<double>(){61.0,64.7,48.6,40.9,36.8,30.2,27.3,23.9,25.2,18.7,15.4,15.6,12.9,10.7,8.5,8.4,9.0,5.7,6.6,6.2,2.3,6.5,3.6,5.5,2.3,3.2,3.5,3.4,3.1,2.2,2.9,2.2,3.4,3.9,5.6,9.1,7.6,6.4,9.8,19.8,30.7,34.5,38.9,45.9,45.5,63.4,73.6,83.7,88.2,96.5,95.6,103.0,112.1,122.7,120.5,117.5,117.8,115.9,118.0,118.8,117.9,126.4,113.8,116.4,106.4,113.9,119.7,114.7,115.4,113.5,116.7,110.9,115.2,118.0,110.3,106.3,100.7,101.3,109.8,112.3,104.3,99.8,98.6,96.0,86.8,87.9,82.9,88.7,88.4,85.7,87.7,88.6,101.7,106.1,105.4,90.3,90.8,89.0,87.6,84.5,82.5,79.8,103.2,100.8,86.4,79.6,82.8,87.7,91.4,98.6,97.3,89.1,84.1,87.8,103.4,120.1,119.2,124.2,119.4,111.7,108.4,105.2,100.4,106.1,110.5,112.0,110.7,107.5,100.3,84.0,86.8,79.6,79.3,75.3,78.9,83.6,89.2,87.8,86.3,80.8,72.9,75.0,70.3,66.0},
+                new List<double>(){8.1,10.2,7.8,7.0,13.3,14.9,12.4,11.0,10.7,8.8,6.7,7.8,10.0,8.2,20.3,36.0,29.9,26.8,21.6,46.0,23.7,19.5,13.7,7.5,6.1,5.6,7.3,8.0,5.3,4.0,3.4,4.5,2.8,6.6,8.0,3.2,8.1,7.2,3.7,5.2,7.9,18.0,16.8,22.4,19.3,18.3,17.2,13.8,21.3,22.9,19.0,32.4,31.6,25.7,23.0,22.6,29.9,30.4,27.3,33.7,27.2,29.0,28.1,31.7,35.5,27.9,21.1,22.3,24.0,21.4,16.7,14.4,21.1,20.3,21.6,22.4,16.7,20.3,12.1,8.6,19.7,24.3,20.3,17.4,13.2,21.3,21.6,16.7,14.4,18.7,20.9,16.6,10.0,7.2,7.3,9.1,10.8,14.3,19.2,18.4,25.8,16.3,14.0,18.2,12.7,17.2,20.6,17.8,24.2,30.3,32.2,24.4,15.6,15.9,17.5,19.5,29.5,24.6,16.3,26.0,20.8,19.2,21.1,27.6,21.5,27.0,24.9,37.4,25.7,29.0,21.1,14.1,21.0,17.6,21.1,15.0,9.7,6.7,9.3,6.8,11.0,11.2,11.2,6.9}
+            };
+            int uniqueTimeSlots = 24; //Also to go in config file - 1 hour slots
+
+            //Creating demand curves and filling them with default values so that they can be accessed through indexes
+            List<List<double>> bucketedDemandCurves = Enumerable.Repeat(new List<double>(uniqueTimeSlots), DEMAND_CURVE.Count).ToList();
+            List<double> totalDemandValues = Enumerable.Repeat(new double(), DEMAND_CURVE.Count).ToList();
+
+            //Re-creating bucketing system like how the initial implementation has it
+            for (int i = 0; i < DEMAND_CURVE.Count; i++)
+            {
+                List<double> bucketedCurve = Enumerable.Repeat(new double(), uniqueTimeSlots).ToList();
+                int bucket = 0;
+                int bucketFill = 0;
+                for (int j = 0; j < DEMAND_CURVE[i].Count; j++)
+                {
+                    bucketedCurve[bucket] = bucketedCurve[bucket] + DEMAND_CURVE[i][j];
+                    bucketFill++;
+                    if (bucketFill == 6)
+                    {
+                        bucketedCurve[bucket] = Math.Round(bucketedCurve[bucket] * 10.0) / 10.0;
+                        bucketFill = 0;
+                        bucket++;
+                    }
+                }
+                bucketedDemandCurves[i] = bucketedCurve;
+
+                //Calculating total demand
+                double totalDemand = 0;
+                for (int j = 0; j < bucketedCurve.Count; j++)
+                {
+                    totalDemand = totalDemand + bucketedCurve[j];
+                }
+
+                totalDemand = Math.Round(totalDemand * 10.0) / 10.0;
+                totalDemandValues[i] = totalDemand;
+            }
+
+
+            var env = new EnvironmentMas(noTurns: 100);
+
+            //Demand curves will be used for requesting and receiving time slot allocations
+            env.Memory.Add("DemandCurve", bucketedDemandCurves);
+            env.Memory.Add("TotalDemandValues", totalDemandValues);
+
             var advertAgent = new AdvertisingAgent(3);
             var houseAg = new HouseAgent(new SelfishBehaviour());
 
@@ -31,7 +78,6 @@ namespace HonoursProject
             env.Add(houseAg, "house");
 
             env.Start();
-            //Console.ReadLine();
         }
     }
 }
