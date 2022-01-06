d2d6a81521501e04b8c2b360f1899b69abb5a43b
Sam Laurenson <40398765@live.napier.ac.uk>
2022-01-06 16:00:16 +0000

    Adding implementation for selfish agent consider request function

--- HonoursProject/behaviours/SelfishBehaviour.cs
+++ HonoursProject/behaviours/SelfishBehaviour.cs
@@ -10,7 +10,26 @@ namespace HonoursProject.behaviours
         //If agent accepts the exchange, true will be returned, otherwise function will return false
         public bool ConsiderRequest(HouseAgent agent, string requestingAgentName, int requestingAgentSlot, int requestedSlot)
         {
-            throw new NotImplementedException();
+            double currentSatisfaction = agent.CalculateSatisfaction(null);
+
+            List<int> potentialAllocatedTimeSlots = new List<int>(agent.AllocatedSlots);
+
+            //Checking agent still has time slot that requesting agent is looking for
+            if (potentialAllocatedTimeSlots.Contains(requestedSlot))
+            {
+                //Replacing requested slot with the requesting agents unwanted slot
+                potentialAllocatedTimeSlots.Remove(requestedSlot);
+                potentialAllocatedTimeSlots.Add(requestingAgentSlot);
+
+                double potentialSatisfaction = agent.CalculateSatisfaction(potentialAllocatedTimeSlots);
+
+                //Selfish agents only accept offers that improve their current satisfaction
+                if (potentialSatisfaction > currentSatisfaction)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         //Determines if the agent should switch strategy from selfish to social
