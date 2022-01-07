c0cb9d69bd26d2e53802446323c70d2fee5a8340
Sam Laurenson <40398765@live.napier.ac.uk>
2022-01-07 15:25:33 +0000

    Creating functionality for advertiser agent to create a dictionary of house agent names and slots they want to advertise

--- HonoursProject/AdvertisingAgent.cs
+++ HonoursProject/AdvertisingAgent.cs
@@ -8,6 +8,8 @@ namespace HonoursProject
     class AdvertisingAgent : Agent
     {
         private int _numberOfExchangeRounds;
+        private Dictionary<string, List<int>> _advertisedTimeSlots = new Dictionary<string, List<int>>();
+
         public AdvertisingAgent(int numberOfExchangeRounds)
         {
             this._numberOfExchangeRounds = numberOfExchangeRounds;
@@ -27,6 +29,13 @@ namespace HonoursProject
                 switch (action)
                 {
                     case "list":
+                        string nameOfAdvertiser = parameters[0];
+                        List<int> advertisedSlots = new List<int>();
+                        for (int i = 1; i < parameters.Count; i++)
+                        {
+                            advertisedSlots.Add(Int32.Parse(parameters[i]));
+                        }
+                        this._advertisedTimeSlots.Add(nameOfAdvertiser, advertisedSlots);
                         break;
                     case "request":
                         break;
