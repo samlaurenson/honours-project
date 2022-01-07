078a999fa73d49c94054bba23b9c40ccb25e769c
Sam Laurenson <40398765@live.napier.ac.uk>
2022-01-07 12:15:04 +0000

    Adding data store singleton

---
+++ HonoursProject/DataStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HonoursProject
+{
+    class DataStore
+    {
+        private static DataStore _instance;
+
+        private DataStore() {}
+
+        public static DataStore Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DataStore();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
