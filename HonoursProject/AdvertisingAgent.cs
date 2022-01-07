0d676494f39a37fea902c066f437db35ef0fdd12
Sam Laurenson <40398765@live.napier.ac.uk>
2022-01-07 15:37:44 +0000

    Adding functionality to advertising agent to allow house agents to request an advertised slot

--- HonoursProject/AdvertisingAgent.cs
+++ HonoursProject/AdvertisingAgent.cs
@@ -38,6 +38,32 @@ namespace HonoursProject
                         this._advertisedTimeSlots.Add(nameOfAdvertiser, advertisedSlots);
                         break;
                     case "request":
+                        int desiredSlot = Int32.Parse(parameters[0]);
+                        int slotToExchange = Int32.Parse(parameters[1]);
+
+                        string agentToContact = "";
+                            
+                        foreach (KeyValuePair<string, List<int>> agent in this._advertisedTimeSlots)
+                        {
+                            if (agent.Value.Contains(desiredSlot))
+                            {
+                                agentToContact = agent.Key;
+                                agent.Value.Remove(desiredSlot);
+                                break;
+                            }
+                        }
+
+                        //Sending message to agent with the desired slot which contains:
+                        // - the name of the house agent that wants the slot
+                        // - the slot the requesting agent would want to exchange for the desired slot
+                        // - the desired slot the requesting agent wants 
+                        Send(agentToContact, $"sendRequest {message.Sender} {slotToExchange} {desiredSlot}");
+                        break;
+                    case "requestUnsuccessful":
+                        //If the exchange did not happen, then a message will be sent back to the advertising agent which contains the slot
+                        //to add back to the list of advertised slots of the agent that had the desired slot
+                        int slotToReAdd = Int32.Parse(parameters[0]);
+                        this._advertisedTimeSlots[message.Sender].Add(slotToReAdd);
                         break;
                 }
             }
