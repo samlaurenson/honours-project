5d0fd41e9b85db0518ec878249883876a875658f
Sam Laurenson <40398765@live.napier.ac.uk>
2022-01-19 13:09:52 +0000

    Adding in unit test to check that slots are being removed from advertised list when slot is requested

--- HonoursProjectTests/AdvertisingAgentTests.cs
+++ HonoursProjectTests/AdvertisingAgentTests.cs
@@ -73,5 +73,42 @@ namespace HonoursProject.Tests
 
             Assert.AreEqual(expected, message);
         }
+
+        [TestMethod()]
+        public void SendRequestTest_ListLengthCheck()
+        {
+            //Testing that requests are removing the requested slot from the list of advertised slots -- ensures that the slot cannot be requested more than once
+
+            //Initialising house agents 
+            var house0 = new HouseAgent(new SelfishBehaviour(), 0);
+            house0.Name = "house0";
+            house0.AllocatedSlots = new List<int>() { 1, 2, 3, 4 };
+            house0.RequestedSlots = new List<int>() { 6, 2, 3, 9 };
+
+            var house1 = new HouseAgent(new SelfishBehaviour(), 1);
+            house1.Name = "house1";
+            var house2 = new HouseAgent(new SelfishBehaviour(), 2);
+            house2.Name = "house2";
+
+            //Adding houses to list of house agents in datastore
+            DataStore dataStore = DataStore.Instance;
+            dataStore.HouseAgents.Add(house0);
+            dataStore.HouseAgents.Add(house1);
+            dataStore.HouseAgents.Add(house2);
+
+            //Setting up advertising agent
+            var advert = new AdvertisingAgent(3);
+            advert.CurrentAdvertisingHouse = "house0";
+
+            advert.ListHouseTimeSlots("house0", new List<string>() { "1", "4" });
+
+            advert.Requests.Add(new Tuple<string, int, int>("house1", 1, 6));   //House 1 sent request for slot 1 that House 0 has in exchange for slot 6
+            advert.Requests.Add(new Tuple<string, int, int>("house2", 4, 9));   //House 2 sending request for slot 4 - but house1 sent request first so they will have their exchange considered
+
+            string message = advert.RequestHandler();
+
+            //Should only have 1 slot left after requested slot was removed from the advertised slot list
+            Assert.AreEqual(1, advert.AdvertisedSlots["house0"].Count);
+        }
     }
 }
